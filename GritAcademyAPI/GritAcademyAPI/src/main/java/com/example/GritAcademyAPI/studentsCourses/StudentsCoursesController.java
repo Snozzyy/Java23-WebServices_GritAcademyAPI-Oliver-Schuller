c1265efe80fba1163a31548c4e4e9aa2d6afd880
Snozzyy <oliverschuller@hotmail.com>
2024-03-22 19:29:28 +0100

    feat: add and remove associations

--- GritAcademyAPI/GritAcademyAPI/src/main/java/com/example/GritAcademyAPI/studentsCourses/StudentsCoursesController.java
+++ GritAcademyAPI/GritAcademyAPI/src/main/java/com/example/GritAcademyAPI/studentsCourses/StudentsCoursesController.java
@@ -1,4 +1,38 @@
 package com.example.GritAcademyAPI.studentsCourses;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class StudentsCoursesController {
+    @Autowired
+    StudentsCoursesService studentsCoursesService;
+
+    @GetMapping(value = "/associations", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> getAssociations() {
+        List<StudentsCourses> studentsCourses = studentsCoursesService.findAll();
+        return new ResponseEntity<>(studentsCourses, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/associations/create")
+    public ResponseEntity<Object> saveAssociation(
+            @RequestParam(value = "student_id") Long studentId,
+            @RequestParam(value = "course_id") Long courseId) {
+
+        StudentsCourses studentsCourses = studentsCoursesService.saveAssociation(studentId, courseId);
+        return new ResponseEntity<>(studentsCourses, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/associations/delete/{associationId}")
+    public ResponseEntity<Object> deleteAssociation(@PathVariable(value = "associationId") Long associationId) {
+        return new ResponseEntity<>(studentsCoursesService.deleteById(associationId), HttpStatus.OK);
+    }
 }
--- GritAcademyAPI/GritAcademyAPI/src/main/java/com/example/GritAcademyAPI/studentsCourses/StudentsCoursesService.java
+++ GritAcademyAPI/GritAcademyAPI/src/main/java/com/example/GritAcademyAPI/studentsCourses/StudentsCoursesService.java
@@ -1,4 +1,32 @@
 package com.example.GritAcademyAPI.studentsCourses;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Service
 public class StudentsCoursesService {
+    @Autowired
+    StudentsCoursesRepository studentsCoursesRepository;
+
+    public List<StudentsCourses> findAll() {
+        return studentsCoursesRepository.findAll();
+    }
+
+    public StudentsCourses saveAssociation(Long studentId, Long courseId) {
+        StudentsCourses studentsCourses = new StudentsCourses();
+        studentsCourses.setStudentsId(studentId);
+        studentsCourses.setCoursesId(courseId);
+
+        return studentsCoursesRepository.save(studentsCourses);
+    }
+
+    public HashMap<String, String> deleteById(Long associationId) {
+        studentsCoursesRepository.deleteById(Math.toIntExact(associationId));
+        HashMap<String, String> message = new HashMap<>();
+        message.put("Message", "Association with id " + associationId + " removed");
+        return message;
+    }
 }
